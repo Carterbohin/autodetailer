92df12d29d342877ad992ec36f87bfc5e2d35cd9
Carter Bohin <carterbohin@gmail.com>
2024-10-15 11:30:30 -0600

    added stuff for reviews

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open');
     navElement.classList.toggle('open');
 });
+
+import { reviews } from '../data/reviews.js';
+console.log(reviews)
+
+const myTarget = document.querySelector("#cards");
+
+for(let x =0; x < reviews.length; x++) {
+
+        const mySection = document.createElement('section')
+        const myName = document.createElement('h3')
+        myName.textContent = reviews[x].name
+
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+    
+    mySection.appendChild(myName);
+
+    mySection.appendChild(myReview);
+
+    myTarget.appendChild(mySection);
+    
+
+}//end of loop
