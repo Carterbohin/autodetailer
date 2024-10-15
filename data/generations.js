0bb4b95670926e49c1cd4dcd5c411b374d44c9cb
Carter Bohin <carterbohin@gmail.com>
2024-10-15 11:35:46 -0600

    added text

---
+++ data/generations.js
@@ -0,0 +1,54 @@
+export const genData = [
+
+
+    {
+        id: 1,
+        name: "John D.",
+        rating: 5,
+        comment: "Amazing service! My car looks brand new after the detailing. Will definitely come back.",
+        date: "2024-10-10",
+        year: 2024,
+        version: "v1.0",
+        info: "Full interior and exterior detailing"
+      },
+      {
+        id: 2,
+        name: "Emily R.",
+        rating: 4,
+        comment: "Great attention to detail. The interior cleaning was spotless. Only reason for 4 stars is that it took a little longer than expected.",
+        date: "2024-10-12",
+        year: 2024,
+        version: "v1.1",
+        info: "Interior detailing only"
+      },
+      {
+        id: 3,
+        name: "Michael S.",
+        rating: 5,
+        comment: "Super professional and efficient. They even removed scratches I thought were permanent. Highly recommend!",
+        date: "2024-10-14",
+        year: 2024,
+        version: "v1.0",
+        info: "Exterior scratch removal and polish"
+      },
+      {
+        id: 4,
+        name: "Samantha W.",
+        rating: 5,
+        comment: "The best detailing experience I've had. My car hasn't looked this good in years!",
+        date: "2024-10-08",
+        year: 2024,
+        version: "v1.2",
+        info: "Full detailing package"
+      },
+      {
+        id: 5,
+        name: "David P.",
+        rating: 4,
+        comment: "Really great service, but they were a little late on the pickup. Otherwise, the car looks fantastic.",
+        date: "2024-10-09",
+        year: 2024,
+        version: "v1.0",
+        info: "Full exterior detailing"
+      }
+    ];
